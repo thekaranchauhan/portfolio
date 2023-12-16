f510f756cd98b3b67a0551570915cd21a91f0f1c
thekaranchauhan <chauhankaran2399@gmail.com>
2023-12-16 14:14:21 -0500

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import PreLoader from "./animations/Preloader/Preloader";
+import Navbar from "./navbar/Navbar";
 
 export default function Home() {
   return (
     <>
     <PreLoader />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hi
-    </main>
+    <Navbar />
     </>
   )
 }
