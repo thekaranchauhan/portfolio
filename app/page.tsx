a75c75b92c2a0f5a7c27b85d38391497e9cd26b2
thekaranchauhan <chauhankaran2399@gmail.com>
2023-12-16 13:52:42 -0500

    Created preloader animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+"use client";
+
+import PreLoader from "./animations/Preloader/Preloader";
 
 export default function Home() {
   return (
+    <>
+    <PreLoader />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hi
     </main>
+    </>
   )
 }
