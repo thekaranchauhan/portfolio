49b22dd03c7eb4144732b394786b4a72572f12b1
thekaranchauhan <chauhankaran2399@gmail.com>
2023-12-18 18:29:20 -0500

    Spelling corrections

--- app/Snippets/Snippets.tsx
+++ app/Snippets/Snippets.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import SnippetGrid from "./SnippetGrid";
 
-const Snips = () => {
+const Snippets = () => {
   return (
     <section
       className="relative z-10 flex w-full flex-col items-center justify-center bg-[#0E1016] bg-cover bg-center py-16 md:py-20 lg:py-20"
@@ -11,4 +12,4 @@ const Snips = () => {
   );
 };
 
-export default Snips;
+export default Snippets;
